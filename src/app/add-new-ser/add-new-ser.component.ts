705bb97885e2fa38435918f32d2b72f58ca49523
Ahmed Ghoraba <ahmedghoraba2008@gmail.com>
2018-06-09 15:57:32 +0200

    Ahmed Comps

---
+++ src/app/add-new-ser/add-new-ser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-ser',
+  templateUrl: './add-new-ser.component.html',
+  styleUrls: ['./add-new-ser.component.scss']
+})
+export class AddNewSerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/add-service4/add-service4.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-service4',
+  templateUrl: './add-service4.component.html',
+  styleUrls: ['./add-service4.component.scss']
+})
+export class AddService4Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
