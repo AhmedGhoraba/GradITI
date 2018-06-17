f3ec51f5bcc49649f05b448e5a2863532b3c0b71
HudaSalah <hudasalah29@gmail.com>
2018-06-17 16:08:19 +0200

    rest of routes done (post req and new req button)

--- src/app/my-requests/my-requests.component.ts
+++ src/app/my-requests/my-requests.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-requests',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyRequestsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private OurRoute:Router) { }
+
+
+  //navigate (routes)
+  redirectToAddReq():void
+  {
+    this.OurRoute.navigateByUrl('/PostRequest');
+  }
 
   ngOnInit() {
   }
